f9507144689e9576d2b08f21dceeff2f64c98328
mynamebvh <mynamebvh@gmail.com>
2024-01-21 00:43:26 +0700

    update api live

--- src/controllers/live.controller.js
+++ src/controllers/live.controller.js
@@ -5,15 +5,27 @@ const catchAsync = require("../utils/catchAsync");
 const ApiError = require("../utils/ApiError");
 
 const onConnect = catchAsync(async (req, res) => {
-  liveKey = req.body.app
-  if (liveKey != "live" && liveKey != "transcode_live"){
+  let {app: liveKey, swfurl} = req.body
+
+  if(Array.isArray(liveKey)){
+    liveKey = liveKey[0]
+  }
+
+  if (liveKey != "live" && liveKey != "transcode_live" && swfurl != ""){
     throw new ApiError(httpStatus.BAD_REQUEST, "app not found");
   }
   res.status(httpStatus.OK).send({ code: httpStatus.OK, message: "success", data: null, error: "" });
 });
 
 const onPlay = catchAsync(async (req, res) => {
-  // console.log("on_play ", req.body);
+  let { token, secret } = req.body
+  if(secret && secret == process.env.SECRET_TRANSCODE){
+    res.status(httpStatus.OK).send({ code: httpStatus.OK, message: "success", data: null, error: "" });
+  }
+
+  if(!token){
+    throw new ApiError(httpStatus.BAD_REQUEST, "token not valid");
+  }
   res.status(httpStatus.OK).send({ code: httpStatus.OK, message: "success", data: null, error: "" });
 });
 
@@ -27,7 +39,7 @@ const onPublish = catchAsync(async (req, res) => {
 });
 
 const onDone = catchAsync(async (req, res) => {
-  console.log("on_done ", req.body);
+  // console.log("on_done ", req.body);
   res.status(httpStatus.OK).send({ code: httpStatus.OK, message: "success", data: null, error: "" });
 });
 
@@ -37,12 +49,12 @@ const onPlayDone = catchAsync(async (req, res) => {
 });
 
 const onPublishDone = catchAsync(async (req, res) => {
-  console.log("on_publish_done ", req.body);
+  // console.log("on_publish_done ", req.body);
   res.status(httpStatus.OK).send({ code: httpStatus.OK, message: "success", data: null, error: "" });
 });
 
 const onRecordDone = catchAsync(async (req, res) => {
-  console.log("on_record_done ", req.body);
+  // console.log("on_record_done ", req.body);
   res.status(httpStatus.OK).send({ code: httpStatus.OK, message: "success", data: null, error: "" });
 });
 
